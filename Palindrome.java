755513e6fa017c2426ff36cac0605fbeb718588d
Stephin-21 <109020237+Stephin-21@users.noreply.github.com>
2023-10-03 15:15:46 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,37 @@
+/*
+ * File : Palindrome
+ * Description : Checking whether a String is Palindrome or Not
+ * Author : Stephin Mathew
+ * Version : 1.0
+ * Date :3/10/2023
+ */
+
+package test;
+import java.util.Scanner;
+public class Palindrome {
+ public static void main(String args[]) {
+	 Scanner sc=new Scanner(System.in);
+	 System.out.println("Enter the string : ");
+	 String input = sc.next();
+	 boolean isPalindrome =check(input);
+	 if(isPalindrome) {
+		 System.out.println("The Given string is Palindrome");
+	 }
+	 else {
+		 System.out.println("The Given string is not Palindrome");
+	 }
+ }
+ 
+	static boolean check(String input) {
+		 char[] charArray=input.toCharArray();
+		 int length =input.length();
+		for(int i=0;i<=length/2;i++) {
+			if(charArray[i]!=charArray[length-i-1]) {
+				return false ;
+			}
+		}
+		return true;
+	 }
+}
+ 
+
