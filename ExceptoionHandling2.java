0d073cb36518610d01777ebbfbc568bafce8319e
Stephin Mathew <109020237+Stephin-Mathew@users.noreply.github.com>
2023-11-10 01:11:32 -0800

    Add files via upload

---
+++ ExceptoionHandling2.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.util.Scanner;
+
+public class ExceptoionHandling2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1,num2;
+		char choice='y';
+		while(choice=='y') {
+		System.out.println("Enter Number 1 : ");
+		num1=sc.nextInt();
+		System.out.println("Enter Number 2 : ");
+		num2=sc.nextInt();
+		
+		int result;
+		try {
+			result=num1/num2;
+			System.out.println("Result = "+result);
+			}
+			catch(Exception e){
+			System.out.println("/ by zero");
+		}
+		finally {
+			System.out.println("End Of Operation");
+			System.out.println("Do you want to continue y or n ");
+			choice=sc.next().charAt(0);
+			
+		}
+		
+		}
+	}
+}
+
