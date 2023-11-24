ecd6f2c7c5b3cfcce80e26272cca6c6e88d76677
Stephin Mathew <109020237+Stephin-Mathew@users.noreply.github.com>
2023-11-24 02:54:00 -0800

    Add files via upload

---
+++ FileHandling.java
@@ -0,0 +1,44 @@
+/*
+ * File : FileHandling
+ * Description : Copying data from one file to another
+ * Author : Stephin Mathew
+ * Version : 1.0
+ * Date :24/11/2023
+ */
+
+package test;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+public class FileHandling {
+	 public static void main(String [] args) {
+		FileReader input= null;
+		FileWriter output= null;
+		FileWriter output2=null;
+		char [] array=new char[50];
+		String input1="Hello World";
+		
+		try{
+			output=new FileWriter("input.txt");
+			output.write(input1);
+			output.flush();
+			output.close();
+			
+			
+			input = new FileReader("input.txt");
+			int bytes=input.read(array);
+			input.close();
+			
+			
+			output2 = new FileWriter("output.txt");
+			output2.write(array,0,bytes);
+			output2.flush();
+			output2.close();
+			
+		}catch(IOException e ) {
+			e.printStackTrace();
+		}
+
+	}
+	}
+
