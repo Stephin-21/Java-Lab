857fd58d4d19a0cba0de12c7170647913c74f49f
Stephin Mathew <109020237+Stephin-Mathew@users.noreply.github.com>
2023-11-27 20:21:09 -0800

    Add files via upload

---
+++ SumUsingStringTokenizer.java
@@ -0,0 +1,33 @@
+/*
+ * File : SumUsingStringTokenizer
+ * Description : Calculating sum using String Tokenizer
+ * Author : Stephin Mathew
+ * Version : 1.0
+ * Date :28/11/2023
+ */
+
+package test;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class SumUsingStringTokenizer {
+	public static void main(String[] args) {
+		int sum=0,num;
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter Numbers: ");
+		String input=sc.nextLine();
+		StringTokenizer obj=new StringTokenizer(input);
+		
+		int noOfTokens=obj.countTokens();
+		System.out.println(noOfTokens);
+		
+		while(obj.hasMoreTokens()) {
+			 num=Integer.parseInt(obj.nextToken());
+			 sum=sum+num;
+		}
+		System.out.println("Sum of "+noOfTokens+" Tokens is : "+sum);
+			
+			
+		}
+
+	}
+
