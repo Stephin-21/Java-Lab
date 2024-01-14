26686b7092e1c321822e0156276bcfed298bfed0
Stephin Mathew <109020237+Stephin-Mathew@users.noreply.github.com>
2024-01-14 11:27:33 +0530

    Update AreaCalculator.java

--- AreaCalculator.java
+++ AreaCalculator.java
@@ -9,17 +9,18 @@
 
 package test;
 import java.util.Scanner;
-public class AreaCalculator {
-public static void main(String [] args) {
-float base,height,radius;
-int width,Height;
-Scanner sc= new Scanner(System.in);
-Area area =new Area();
 
-System.out.println("Enter The base of the Triangle : ");
-base=sc.nextFloat();
-System.out.println("Enter The Height of the Triangle : ");
-height=sc.nextFloat();
+public class AreaCalculator {
+  public static void main(String [] args) {
+ 	float base,height,radius;
+ 	int width,Height;
+   	Scanner sc= new Scanner(System.in);
+   	Area area =new Area();
+
+   	System.out.println("Enter The base of the Triangle : ");
+   	base=sc.nextFloat();
+	System.out.println("Enter The Height of the Triangle : ");
+	height=sc.nextFloat();
 
 System.out.println("Enter The Width of the Rectangle : ");
 width=sc.nextInt();
@@ -35,15 +36,16 @@ area.getArea(radius);
 
 }
 }
-class Area{
-public void getArea(float base,float height) {
-	System.out.println("Area of Triangle with base "+base+ " and height "+height+ " is : "+ 0.5*base*height);
-}
 
-public void getArea(int width,int Height) {
+class Area{
+  public void getArea(float base,float height) {
+	System.out.println("Area of Triangle with base "+base+ " and height "+height+ " is : "+ 0.5*base*height);
+  }
+
+  public void getArea(int width,int Height) {
 		System.out.println("Area of Rectangle with width "+width+ " and length "+Height + " is : " +width*Height);
-	}
-public void getArea(float radius) {
+  }
+  public void getArea(float radius) {
 		System.out.println("Area of Circle with radius "+radius+ " is : "+3.14*radius*radius);
-}
+  }
 }
